ccda86f5aed4fca97a90dba8370aa98ba4d43445
rttrif <55245756+rttrif@users.noreply.github.com>
2020-04-06 19:56:13 +0900

    Add files via upload

---
+++ items.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+from scrapy.loader.processors import MapCompose, TakeFirst
+
+
+class BlogparseItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    pass
+
+
+def clean_photo(values):
+    if values[:2] == '//':
+        return f'http:{values}'
+    return values
+
+
+class AvitoRealEstateItem(scrapy.Item):
+    _id = scrapy.Field()
+    url = scrapy.Field(output_processor=TakeFirst())
+    title = scrapy.Field(output_processor=TakeFirst())
+    photos = scrapy.Field(input_processor=MapCompose(clean_photo))
---
+++ pipelines.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+import scrapy
+from scrapy.pipelines.images import ImagesPipeline
+from pymongo import MongoClient
+
+
+class BlogparsePipeline(object):
+    def __init__(self):
+        client = MongoClient('mongodb://localhost:27017/')
+        self.db = client['blog_parse']
+
+    def process_item(self, item, spider):
+        collection = self.db[spider.name]
+        collection.insert_one(item)
+        return item
+
+
+class ImgPipeLine(ImagesPipeline):
+
+    def get_media_requests(self, item, info):
+
+        if item.get('photos'):
+            for img_url in item['photos']:
+                try:
+                    yield scrapy.Request(img_url)
+                except Exception as e:
+                    pass
+
+    def item_completed(self, results, item, info):
+        if results:
+            item['photos'] = [itm[1] for itm in results]
+        return item
+
+
+class TempPipeline:
+    def process_item(self, item, spider):
+        return item
